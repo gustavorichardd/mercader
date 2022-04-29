437910aaff4e57de78275aa35c9b9edbf2d234f4
Gustavo Richard <gustavo.richard@outlook.com.br>
2022-04-29 15:01:40 -0300

    Corrigido para múltiplos mercados.

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ const fs = require('fs');
 const db = require('./database/connections')
 
 
-const superMarkets = {
-   bistekPromo: {
+const superMarkets = [
+   {
+      id: 1,
       url: 'https://www.bistek.com.br/',
+      sections: 'a.navigation__inner-link',
       productDetails: {
          tag: 'div.product-item-info',
          image: 'a > span > span > img',
@@ -17,8 +19,10 @@ const superMarkets = {
          priceClub: '.price-clube-bistek'
       }
    },
-   angeloni: {
-      url: 'https://www.bistek.com.br/',
+   {
+      id: 2,
+      url: 'https://www.deliveryfort.com.br/',
+      sections: 'li.navigation__inner-item > a.navigation__inner-link',
       productDetails: {
          tag: 'div.product-item-info',
          image: 'a > span > span > img',
@@ -28,15 +32,18 @@ const superMarkets = {
          priceClub: '.price-clube-bistek'
       }
    }
-}
+]
 
-async function insertValuePrice({ name, image, price, priceClub = 0 }) {
+async function insertValuePrice({ name, image, price, priceClub = 0, marketId }) {
+   console.log({ name, image, price, priceClub, marketId })
    await db`
      insert into tb_prices
-       (name, image, price, priceClub)
+       (name, image, price, priceclub, id_supermarket)
      values
-       (${name}, ${image}, ${price}, ${priceClub})
+       (${name}, ${image}, ${price}, ${priceClub}, ${marketId})
    `
+
+   return null;
 }
 
 function formatPrice(price) {
@@ -44,55 +51,61 @@ function formatPrice(price) {
    return Number(newPrice.slice(newPrice.length - 5, newPrice.length).replace(',', '.'))
 }
 
-request(superMarkets.bistekPromo.url, function (err, res, body) {
-   if (err) {
-      console.log(err, "error occured while hitting URL");
-   } else {
-      let $ = cheerio.load(body);
-      let validPages = []
-      $('ul.navigation__inner-list').each(function (index) {
-         const page = $(this).find('li.navigation__inner-item > a.navigation__inner-link').attr('href');
-         if (page !== '#')
-            validPages.push(page)
-      })
-      let index = 0
-      const pages = [...new Set(validPages)]
-
-      while (index < pages.length) {
-         console.log(`${pages[index]}?product_list_limit=all`, "PROCESS STARTED.")
-         request(`${pages[index]}?product_list_limit=all`, function (err, res, body) {
-            if (err) {
-               console.log(err, "error occured while hitting URL for specific page: ", pages[index]);
-            } else {
-               let $ = cheerio.load(body);
-
-               $(superMarkets.bistekPromo.productDetails.tag).each(function (index) {
-                  const image = $(this).find(superMarkets.bistekPromo.productDetails.image).attr('src');
-                  const name = $(this).find(superMarkets.bistekPromo.productDetails.name).text();
-                  const price = $(this).find(superMarkets.bistekPromo.productDetails.price).text();
-                  const item = {
-                     image,
-                     name: name.trim(),
-                     price: formatPrice(price),
-                  }
-
-                  if (price === 0) {
-                     const priceSpecial = $(this).find(superMarkets.bistekPromo.productDetails.priceSpecial).text();
-                     const priceClub = $(this).find(superMarkets.bistekPromo.productDetails.priceClub).text();
-                     delete item.price
-                     item.price = formatPrice(priceSpecial)
-                     item.priceClub = formatPrice(priceClub)
-
-                  }
-
-                  insertValuePrice(item)
-               });
+for (const market of superMarkets) {
+   request(market.url, function (err, res, body) {
+      if (err) {
+         console.log(err, "error occured while hitting URL");
+      } else {
+         let $ = cheerio.load(body);
+         let validPages = []
+         $('ul.navigation__inner-list').each(function (index) {
+            const page = $(this).find(market.sections).attr('href');
+            if (page !== '#') {
+               validPages.push(`${page}?product_list_limit=all`)
             }
-         })
 
-         console.log(`${pages[index]}?product_list_limit=all`, "PROCESS FINISHED.")
-         index++
+         })
+         let index = 0
+         const pages = [...new Set(validPages)]
+
+         while (index < pages.length) {
+            console.log(pages[index], "PROCESS STARTED.")
+            request(pages[index], function (err, res, body) {
+               if (err) {
+                  console.log(err, "error occured while hitting URL for specific page: ", pages[index]);
+               } else {
+                  let $ = cheerio.load(body);
+
+                  $(market.productDetails.tag).each(async function () {
+                     const image = $(this).find(market.productDetails.image).attr('src');
+                     const name = $(this).find(market.productDetails.name).text();
+                     const price = $(this).find(market.productDetails.price).text();
+                     const item = {
+                        image,
+                        name: name.trim(),
+                        price: formatPrice(price),
+                     }
+
+                     if (price === 0) {
+                        const priceSpecial = $(this).find(market.productDetails.priceSpecial).text();
+                        const priceClub = $(this).find(market.productDetails.priceClub).text();
+                        console.log(priceClub)
+                        // delete item.price
+                        // item.price = formatPrice(priceSpecial)
+                        item.priceClub = formatPrice(priceClub)
+                     }
+
+                     item.marketId = market.id
+
+                     await insertValuePrice(item)
+                  });
+               }
+            })
+
+            console.log(pages[index], "PROCESS FINISHED.")
+            index++
+         }
+         console.log('PROCESS FINISHED...')
       }
-      console.log('PROCESS FINISHED...')
-   }
-});
+   });
+}
